7cb1792303de0b0c5d9a02749bf5c109b6a90eca
ChenCyl <programming2@163.com>
2019-07-23 23:32:37 +0800

    perf: input score and comment

--- src/pages/teacher/check/index.vue
+++ src/pages/teacher/check/index.vue
@@ -1,52 +1,46 @@
 <template>
-  <div class="wrapper">
-    <div class="pdf">
-      <embed :src="url" type="application/pdf" width="100%" height="100%" />
-    </div>
-    <div class="check">
-      <a-form :form="form">
-        <a-form-item
-          :label-col="formItemLayout.labelCol"
-          :wrapper-col="formItemLayout.wrapperCol"
-          label="Name"
-        >
-          <a-input
-            v-decorator="[
-              'username',
-              { rules: [{ required: true, message: 'Please input your name' }] }
-            ]"
-            placeholder="Please input your name"
-          />
-        </a-form-item>
-        <a-form-item
-          :label-col="formItemLayout.labelCol"
-          :wrapper-col="formItemLayout.wrapperCol"
-          label="Nickname"
-        >
-          <a-input
-            v-decorator="[
-              'nickname',
-              {
-                rules: [
-                  { required: checkNick, message: 'Please input your nickname' }
-                ]
-              }
-            ]"
-            placeholder="Please input your nickname"
-          />
-        </a-form-item>
-        <a-row>
-          <a-col :span="24" :style="{ textAlign: 'right' }">
-            <a-button @click="$router.go(-1)">返回</a-button>
-            <a-button
-              type="primary"
-              @click="check"
-              :style="{ marginLeft: '8px' }"
-              >保存</a-button
-            >
-          </a-col>
-        </a-row>
-      </a-form>
+  <div style="height: 100%">
+    <h1>{{ uid }} 的实验报告</h1>
+    <div class="wrapper">
+      <div class="pdf">
+        <embed :src="url" type="application/pdf" width="100%" height="100%" />
+      </div>
+      <div class="check">
+        <a-form :form="form">
+          <a-form-item
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+            label="分数"
+          >
+            <a-input-number
+              :min="0"
+              :max="100"
+              v-decorator="[
+                'score',
+                { rules: [{ required: true, message: '分数不能为空' }] }
+              ]"
+            />
+          </a-form-item>
+          <a-form-item
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+            label="评价"
+          >
+            <a-textarea :rows="5" />
+          </a-form-item>
+          <a-row>
+            <a-col :span="24" :style="{ textAlign: 'right' }">
+              <a-button @click="$router.go(-1)">返回</a-button>
+              <a-button
+                type="primary"
+                @click="check"
+                :style="{ marginLeft: '8px' }"
+                >保存</a-button
+              >
+            </a-col>
+          </a-row>
+        </a-form>
+      </div>
     </div>
   </div>
 </template>
@@ -64,11 +58,13 @@ export default {
     return {
       url: this.$route.params.url,
       uid: this.$route.params.uid,
-      checkNick: false,
       formItemLayout,
       formTailLayout,
       form: this.$form.createForm(this)
     };
+  },
+  created() {
+    // 上一页离开之前用 mapstate 保存表单内容
   },
   methods: {
     check() {
@@ -76,12 +72,6 @@ export default {
         if (!err) {
           console.info("success");
         }
-      });
-    },
-    handleChange(e) {
-      this.checkNick = e.target.checked;
-      this.$nextTick(() => {
-        this.form.validateFields(["nickname"], { force: true });
       });
     }
   }
